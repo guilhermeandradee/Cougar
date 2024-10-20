c9401550e61cef017fb2f52de153ba9ea99873c4
Guilherme Andrade <96486731+guilhermeandradee@users.noreply.github.com>
2024-10-20 13:27:03 -0300

    alterando instruções da IA

--- backend/services/IAService.js
+++ backend/services/IAService.js
@@ -11,7 +11,7 @@ class IAService {
     async gerar_palavras_chave(description) {
         const assistant = await openai.beta.assistants.create({
         name: "Words Generator",
-        instructions: "Você é um assistente de linguagem natural. Quando receber um texto, siga estas etapas: 1. Analise o texto e identifique os temas principais. 2. Remova palavras irrelevantes (stop words) e termos que não adicionam valor. 3. Aplique lematização para transformar palavras em suas formas base. 4. Selecione as palavras mais significativas e crie uma lista de até 3 palavras-chave. 5. Retorne as palavras-chave em formato JSON. Texto de exemplo: 'O sistema apresentou uma falha durante a atualização.' A saída deve ser: {'palavras-chave': ['sistema','falha', 'atualização', 'problema', 'erro']} E pode ter mais palavras chave.",
+        instructions: "Você é um assistente de linguagem natural. Quando receber um texto, siga estas etapas: 1. Analise o texto e identifique os temas principais. 2. Remova palavras irrelevantes (stop words) e termos que não adicionam valor. 3. Aplique lematização para transformar palavras em suas formas base. 4. Selecione as palavras mais significativas e crie uma lista de palavras-chave. 5. Retorne as palavras-chave em formato JSON. Texto de exemplo: 'O sistema apresentou uma falha durante a atualização.' A saída deve ser: {'palavras-chave': ['sistema','falha', 'atualização', 'problema', 'erro']}",
         tools: [{ type: "code_interpreter" }],
         model: "gpt-3.5-turbo"
     });
